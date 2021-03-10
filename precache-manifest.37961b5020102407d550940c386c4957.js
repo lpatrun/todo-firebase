250c0f88da427f06f931e757aa264d622c54f2b8
lpatrun <luka.patrun@gmail.com>
2021-03-10 19:49:25 +0100

    Updates

---
+++ precache-manifest.37961b5020102407d550940c386c4957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49024493ee2434a83c4c51ba922dae21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15deafef36391bad2d3c",
+    "url": "/static/css/main.61d32d99.chunk.css"
+  },
+  {
+    "revision": "3ed7f47a01c04a74102c",
+    "url": "/static/js/2.0f15d2e8.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.0f15d2e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15deafef36391bad2d3c",
+    "url": "/static/js/main.a54845cf.chunk.js"
+  },
+  {
+    "revision": "82804eebff79ec84af63",
+    "url": "/static/js/runtime-main.0602e5d0.js"
+  }
+]);
